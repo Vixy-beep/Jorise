036211ae461201a1d16e6bca662b6bd64f84c191
Jordano Fermin <rub1r0za@higurama.higurama.com>
2025-10-09 13:51:51 -0400

    Fix build.sh - specify correct Django settings module

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py collectstatic --no-input --settings=jorise_v2_complete.settings
+python manage.py migrate --settings=jorise_v2_complete.settings
